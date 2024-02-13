7dc5dd3cccea148bc4724369d4b677b80759c3a1
Fili_vw23@student.itstep.org <Fili_vw23@student.itstep.org>
2024-02-13 11:55:05 +0200

    getAll and getById User fix

--- MedicalPlus/Controllers/UserController.cs
+++ MedicalPlus/Controllers/UserController.cs
@@ -17,17 +17,51 @@ namespace MedicalPlus.Controllers
     [ApiController]
     public class UserController : Controller
     {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<User> _userManager;
         private readonly IUnitOfWorks _unitOfWorks;
-        public UserController(IUnitOfWorks unitOfWorks)
+        public UserController(IUnitOfWorks unitOfWorks, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
             this._unitOfWorks = unitOfWorks;
+            this._userManager = userManager;
+            this._roleManager = roleManager;
         }
 
         [HttpGet]
         [Route("getAll")]
         public async Task<IActionResult> GetAll()
         {
-            return Ok(this._unitOfWorks.UserRepo.GetAll().Result);
+            var fios = await _unitOfWorks.FioRepo.GetAll();
+            var genders = await _unitOfWorks.GenderRepo.GetAll();
+            List<User> users = await this._unitOfWorks.UserRepo.GetAll();
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            foreach (var user in users)
+            {
+
+                var roles = await _userManager.GetRolesAsync(user);
+                RoleModel role = new RoleModel() { Id = "0", Name = "default" };
+                if (roles.Count != 0)
+                {
+                    var temp = await _roleManager.FindByNameAsync(roles[0]);
+                    role = new RoleModel() { Id = temp.Id, Name = temp.Name };
+                }
+
+                Fio fio = fios.FirstOrDefault(x=>x.IdFio==user.IdFio);
+                Gender gender = genders.FirstOrDefault(x => x.IdGender == user.IdGender);
+                EmployeeModel employee = new EmployeeModel()
+                {
+                    Email = user.Email,
+                    UserId = user.Id,
+                    Role = role,
+                    Password = user.PasswordHash,
+                    CurrentPassword = user.PasswordHash,
+                    UserName = user.UserName,
+                    Fio = new FioModel(fio.IdFio, fio.Name, fio.Surname, fio.Patronymic),
+                    Gender = new GenderModel(gender.IdGender, gender.Name)
+                };
+                employees.Add(employee);
+            }
+            return Ok(employees);
         }
 
 
@@ -35,7 +69,33 @@ namespace MedicalPlus.Controllers
         [Route("getById")]
         public async Task<IActionResult> GetById(string id)
         {
-            return Ok(this._unitOfWorks.UserRepo.GetById(id));
+
+            User user = await this._unitOfWorks.UserRepo.GetById(id);
+
+
+            var roles = await _userManager.GetRolesAsync(user);
+            RoleModel role = new RoleModel() { Id = "0", Name = "default" };
+            if (roles.Count != 0)
+            {
+                var temp = await _roleManager.FindByNameAsync(roles[0]);
+                role = new RoleModel() { Id = temp.Id, Name = temp.Name };
+            }
+
+            EmployeeModel employee = new EmployeeModel()
+            {
+                Email = user.Email,
+                UserId = user.Id,
+                Role = role,
+                Password = user.PasswordHash,
+                CurrentPassword = user.PasswordHash,
+                UserName = user.UserName,
+                Fio = new FioModel(user.IdFioNavigation.IdFio, user.IdFioNavigation.Name, user.IdFioNavigation.Surname, user.IdFioNavigation.Patronymic),
+                Gender = new GenderModel(user.IdGenderNavigation.IdGender, user.IdGenderNavigation.Name)
+            };
+
+
+
+            return Ok(employee);
         }
 
 
